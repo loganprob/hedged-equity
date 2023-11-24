b2fe9ba5358a7f3db9d8f9adfb28f132e5648496
loganprob <56107919+loganprob@users.noreply.github.com>
2023-11-24 10:14:00 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import json
+import requests
+import math
+import xml.etree.ElementTree as ET
+import numpy as np
+import pandas as pd
+import datetime as dt
+from scipy import optimize
+from scipy import interpolate
+from scipy.stats import norm
